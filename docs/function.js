c7661ad796ecf1b184a09dd43693b8fa112c355b
tatsuhara <tthr.shower@gmail.com>
2018-12-17 20:16:34 +0900

    Update function.js

--- docs/function.js
+++ docs/function.js
@@ -26,9 +26,9 @@ clipboard.on('error', function(e) {
 
 
 	//たつんとふ設定
-	var ttntf_url = 'https://secure644.sakura.ne.jp/shower.rash.jp/xx/ddntf/web/DodontoF/DodontoFServer.rb';
+	var ttntf_url = 'https://shower.rash.jp/xx/ddntf/web/DodontoF/DodontoFServer.rb';
 
-	var ttntf_img = "https://secure644.sakura.ne.jp/shower.rash.jp/xx/ddntf/web/imageUploadSpace";
+	var ttntf_img = "https://shower.rash.jp/xx/ddntf/web/imageUploadSpace";
 	var spread_img_URL = "//script.google.com/macros/s/AKfycbyqFm-tKIxQivwp9KVj-5Q_dkyDjZJfFkURxUtwQmFPTjKtkxvu/exec";
 
 
