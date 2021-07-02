339c6e7eb48c19add3d0665bae921b1a2cb4545f
tatsuhara <6765920+273c@users.noreply.github.com>
2021-07-02 10:19:24 +0900

    D特徴を勝手に振った

--- docs/new_chara.js
+++ docs/new_chara.js
@@ -166,7 +166,9 @@
     var dice10_1 = mt.nextInt(1, 6);
     var dice10_2 = mt.nextInt(1, 10);
 
-	get_features(dice10_1, dice10_2);
+	var dice10_3 = mt.nextInt(0, 6);
+
+	get_features(dice10_1, dice10_2, dice10_3);
 
 //	var features_name = features[dice10_1][dice10_2].name;
 //	var features_txt = features[dice10_1][dice10_2].txt;
